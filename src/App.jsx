3a4c802de90c59c816d12145aae9c3e14d64e363
Vyctoria Karina <vyctoria@Vyctorias-MBP.fritz.box>
2023-01-10 17:26:51 +0100

    initial setup of the project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Header } from './components/Header';
+import './styles.css';
+
+function App() {
+
+  return (
+    <div>
+      <Header />
+      <h1>Hello!</h1>
+    </div>
+  )
+}
+
+export default App
