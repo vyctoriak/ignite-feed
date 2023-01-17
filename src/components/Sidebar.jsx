eb8f96ab24cc99681f58d3e86f41927d7c3f6f1c
Vyctoria K <vyctoriak@gmail.com>
2023-01-17 23:46:37 +0100

    remove alt property from img tag

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -12,7 +12,7 @@ export function Sidebar() {
       <div className={styles.profile}>
         <img src="https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80" 
         className={styles.avatar}
-        alt="user profile" />
+        />
         <strong>John Doe</strong>
         <span>Frontend Engineer</span>
       </div>
